2d5f6ef8fb804bd06237d891fae34f9bc8a4abf6
Leon Tea <leon.tea@hotmail.com>
2019-10-10 14:40:34 +0200

    added method initClient()

--- src/test/java/at/htl/rest/TimeServerIT.java
+++ src/test/java/at/htl/rest/TimeServerIT.java
@@ -1,4 +1,19 @@
 package at.htl.rest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 public class TimeServerIT {
+    private Client client;
+    private WebTarget target;
+
+    @BeforeEach
+    public void initClient(){
+        this.client = ClientBuilder.newClient();
+        this.target = client.target("http://localhost:8080/restprimer/api/time");
+    }
 }
